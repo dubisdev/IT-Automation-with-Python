1e82e5d64a527fced60cdb8d85c9606d11086a0b
dubisdev <dubisdev@gmail.com>
2022-07-31 13:39:27 +0200

    (course 1): add week 1

---
+++ 01 - Crash course on Python 🐍/Week 1/quiz.py
@@ -0,0 +1,24 @@
+# 1. Fill in the correct Python command to put “My first Python program” onto the screen.
+print("My first Python program")
+
+
+# 2. Python is an example of what type of programming language?
+# ✅ General purpose scripting language
+
+
+# 3. Convert this Bash command into Python: `echo "Have a nice day"`
+print("Have a nice day")
+
+
+# 4. Fill in the correct Python commands to put “This is fun!” onto the screen 5 times.
+for i in range(5):
+  print("This is fun!")
+
+
+# 5. Select the Python code snippet that corresponds to the following Javascript snippet:
+# for (let i = 0; i < 10; i++) {
+#         console.log(i);
+# }
+
+for i in range(10):
+  print(i)
---
+++ 01 - Crash course on Python 🐍/Week 1/video_questions.py
@@ -0,0 +1,7 @@
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
+
+
+for i in range(10):
+  print(i, "Hello, World!")
